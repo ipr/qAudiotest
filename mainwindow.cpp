1b262581c75138d9a19700dbf5c3b6c4c0e386f7
Ilkka Prusi <ilkka.prusi@gmail.com>
2011-05-24 06:29:43 +0300

    gui-stuff mostly to help testing

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,10 +27,13 @@ MainWindow::MainWindow(QWidget *parent) :
 	// on selection from list
 	//connect(ui->listWidget, SIGNAL(itemActivated(QListWidgetItem*)), this, 
 
+	ui->horizontalSlider->hide();
+	
 	// if file given in command line
 	QStringList vCmdLine = QApplication::arguments();
 	if (vCmdLine.size() > 1)
 	{
+		ui->listWidget->addItem(new QListWidgetItem(vCmdLine[1]));
 		emit FileSelection(vCmdLine[1]);
 	}
 }
@@ -73,6 +76,7 @@ void MainWindow::onFileSelected(QString szFile)
 	std::wstring szFilename = szFile.toStdWString();
 	if (FileTmp.Create(szFilename.c_str()) == false)
 	{
+		ui->statusBar->showMessage("Failed to determine file type");
 		return;
 	}
 	Type.DetermineFileType((uint8_t*)FileTmp.GetView(), 16);
@@ -94,12 +98,14 @@ void MainWindow::onFileSelected(QString szFile)
 	if (m_pAudioFile == nullptr)
 	{
 		// could not determine file type/not supported (yet)
+		ui->statusBar->showMessage("Unknown/unsupported file type");
 		return;
 	}
 	
 	if (m_pAudioFile->ParseFile(szFilename) == false)
 	{
 		// failed opening/processing, not supported type?
+		ui->statusBar->showMessage("Failure reading file");
 		return;
 	}
 	
@@ -128,58 +134,64 @@ void MainWindow::onFileSelected(QString szFile)
 	{
 		format.setSampleType(QAudioFormat::UnSignedInt);
 	}
+	
+	if (format.isValid() == false)
+	{
+		ui->statusBar->showMessage("Invalid audio-format");
+		return;
+	}
 
 	double nFrame = ( format.channels() * format.sampleSize() / 8 );
     double usInBuffer = ( (m_pAudioFile->sampleDataSize()*1000000ui64) / nFrame ) / format.frequency();
-	double dBuf = nFrame * format.frequency(); 
+	double dBuf = nFrame * format.frequency(); // should be size in bytes for one second
 	ui->horizontalSlider->setMaximum(usInBuffer/1000); // -> msec
 	ui->horizontalSlider->setMinimum(0);
 	ui->horizontalSlider->setValue(0);
+
+	m_nWritten = 0;
+	m_pSampleData = (char*)m_pAudioFile->sampleData();
+	m_nSampleSize = m_pAudioFile->sampleDataSize();
 	
 	QAudioDeviceInfo info(QAudioDeviceInfo::defaultOutputDevice());
-	if (info.isFormatSupported(format)) 
+	if (info.isFormatSupported(format) == false) 
 	{
-		m_pAudioOut = new QAudioOutput(format, this);
-		connect(m_pAudioOut, SIGNAL(stateChanged(QAudio::State)), this, SLOT(onAudioState(QAudio::State)));
-		connect(m_pAudioOut, SIGNAL(notify()), this, SLOT(onPlayNotify()));
-
-		// test, increase buffering for smoother playback
-		// on IO-load (Windows trashes disk often..)
-		int iBufSize = m_pAudioOut->bufferSize();
-		if (iBufSize < dBuf)
-		{
-			m_pAudioOut->setBufferSize(dBuf);
-		}
-		
-		// pull-mode (implement the interface needed in audio-file?)
-		//m_pAudioOut->start(m_pAudioFile);
-
-		m_pAudioOut->setNotifyInterval(250);
-		
-		// test: push-mode (need threading to work correctly)
-		m_pDevOut = m_pAudioOut->start();
-		
-		// test, increase priority of playback-thread
-		// to reduce problems on heavy loads
-		QThread *pCur = QThread::currentThread();
-		pCur->setPriority(QThread::TimeCriticalPriority);
-		
-		// TODO: make buffering of file anyway instead of memory-mapped?
-		// experiencing "pauses" under IO-load on Windows..
-		// or just make thread higher-priority?
-		
-		m_nWritten = 0;
-		m_pSampleData = (char*)m_pAudioFile->sampleData();
-		m_nSampleSize = m_pAudioFile->sampleDataSize();
-		
-		qint64 nWritten = m_pDevOut->write(m_pSampleData, m_nSampleSize);
-		if (nWritten == -1)
-		{
-			return;
-		}
-		m_nWritten += nWritten;
+		ui->statusBar->showMessage("Unsupported audio-format");
+		return;
 	}
-		
+	
+	m_pAudioOut = new QAudioOutput(format, this);
+	connect(m_pAudioOut, SIGNAL(stateChanged(QAudio::State)), this, SLOT(onAudioState(QAudio::State)));
+	connect(m_pAudioOut, SIGNAL(notify()), this, SLOT(onPlayNotify()));
+	
+	// test, increase buffering for smoother playback
+	// on IO-load (Windows trashes disk often.. keep more in play-buffer)
+	int iBufSize = m_pAudioOut->bufferSize();
+	if (iBufSize < dBuf)
+	{
+		m_pAudioOut->setBufferSize(dBuf);
+	}
+	
+	// pull-mode (implement the interface needed in audio-file?)
+	//m_pAudioOut->start(m_pAudioFile);
+	
+	m_pAudioOut->setNotifyInterval(250);
+	
+	// test: push-mode (need threading to work correctly)
+	m_pDevOut = m_pAudioOut->start();
+	
+	// test, increase priority of playback-thread
+	// to reduce problems on heavy loads (buffer exhaustion)
+	QThread *pCur = QThread::currentThread();
+	pCur->setPriority(QThread::TimeCriticalPriority);
+	
+	// write initial
+	qint64 nWritten = m_pDevOut->write(m_pSampleData, m_nSampleSize);
+	if (nWritten == -1)
+	{
+		on_actionStop_triggered();
+		return;
+	}
+	m_nWritten += nWritten;
 }
 
 void MainWindow::onAudioState(QAudio::State enState)
@@ -198,13 +210,6 @@ void MainWindow::onAudioState(QAudio::State enState)
 // triggered on certain intervals (set to output-device)
 void MainWindow::onPlayNotify()
 {
-	// temp! testing only..
-	// !! slow way
-	//int iInterval = m_pAudioOut->notifyInterval();
-	int iValue = ui->horizontalSlider->value();
-	ui->horizontalSlider->setValue(iValue + 250);
-	// /temp!
-	
 	// test: only write on intervals (when some buffer has been consumed)
 	// to reduce CPU-load (no need write when buffer is nearly full..)
 	qint64 nWritten = m_pDevOut->write(m_pSampleData + m_nWritten, m_nSampleSize - m_nWritten);
@@ -214,6 +219,10 @@ void MainWindow::onPlayNotify()
 		return;
 	}
 	m_nWritten += nWritten;
+	
+	// TODO: catch user positioning of slider..
+	int iValue = ui->horizontalSlider->value();
+	ui->horizontalSlider->setValue(iValue + 250);
 }
 
 void MainWindow::on_actionFile_triggered()
@@ -221,12 +230,45 @@ void MainWindow::on_actionFile_triggered()
 	QString szFile = QFileDialog::getOpenFileName(this, tr("Open file"));
 	if (szFile != NULL)
 	{
-		emit FileSelection(szFile);
+		// TODO:
+		// if already playing sound -> just add to list
+		// otherwise, start playback
+
+		// (temp)
+		ui->listWidget->addItem(new QListWidgetItem(szFile));
+		
+		// TODO: check audio-state instead?
+		if (m_pAudioOut == nullptr)
+		{
+			emit FileSelection(szFile);
+		}
 	}
 }
 
 void MainWindow::on_actionPlay_triggered()
 {
+	QListWidgetItem *pItem = ui->listWidget->currentItem();
+	if (pItem == nullptr)
+	{
+		return;
+	}
+	
+	// TODO: check audio-state instead?
+	if (m_pAudioOut != nullptr)
+	{
+		// already playing?
+		//return;
+		on_actionStop_triggered();
+	}
+	
+	emit FileSelection(pItem->text());
+	
+	// TODO:
+	// if not already playing,
+	// get current selection in list
+	// and start playback
+	//emit FileSelection(szFile);
+	
 	/*
 	if (m_pAudioOut != nullptr)
 	{
@@ -234,11 +276,13 @@ void MainWindow::on_actionPlay_triggered()
 	}
 	*/
     
-	//m_pAudioOut->start(&m_File);
 }
 
 void MainWindow::on_actionStop_triggered()
 {
+	// TODO: deleted automatically by output-device?
+	m_pDevOut = nullptr;
+	
 	if (m_pAudioOut != nullptr)
 	{
 		m_pAudioOut->stop();
