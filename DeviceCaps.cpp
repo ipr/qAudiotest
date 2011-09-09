9e13509257cf1d6792020785ab5d100fbfb05c80
Ilkka Prusi <ilkka.prusi@gmail.com>
2011-09-10 00:31:50 +0300

    actual listing..

--- DeviceCaps.cpp
+++ DeviceCaps.cpp
@@ -13,13 +13,15 @@ DeviceCaps::DeviceCaps(QWidget *parent) :
     
     QStringList headers;
 	headers << "Device"
-            << "Byteorder"
-            << "Channel count"
+            << "Capability"
+            << "Value";
+    /*
             << "Frequency"
             << "Sample rate"
             << "Sample size"
             << "Sample type"
             << "Codec";
+            */
     
     /*
     << "device: " << info.deviceName()
@@ -32,8 +34,8 @@ DeviceCaps::DeviceCaps(QWidget *parent) :
     << "codec: " << format.codec();
     */
 
-    ui->tableWidget->setColumnCount(treeHeaders.size());    
-	ui->tableWidget->setHeaderLabels(headers);
+    ui->treeWidget->setColumnCount(headers.size());    
+	ui->treeWidget->setHeaderLabels(headers);
     
     showDeviceCaps();
 }
@@ -60,54 +62,130 @@ void DeviceCaps::showDeviceCaps()
         // list device capabilities
         showDevice(info);
     }
-    
+    ui->treeWidget->expandAll();
 }
 
 void DeviceCaps::showDevice(QAudioDeviceInfo &info)
 {
-    qDebug() << "device: " << info.deviceName();
+    //qDebug() << "device: " << info.deviceName();
     
-    qDebug() << "endianess:";
+    QTreeWidgetItem *pCapItem = nullptr;
+    QTreeWidgetItem *pDevice = new QTreeWidgetItem((QTreeWidgetItem*)0);
+    pDevice->setText(0, info.deviceName());
+    ui->treeWidget->addTopLevelItem(pDevice);
+    
+    //qDebug() << "endianess:";
+    pCapItem = new QTreeWidgetItem(pDevice);
+    pCapItem->setText(1, "endianess");
+    
     foreach (QAudioFormat::Endian end, info.supportedByteOrders())
     {
-        qDebug() << (int)end;
+        //qDebug() << (int)end;
+        QTreeWidgetItem *pValItem = new QTreeWidgetItem(pCapItem);
+        if (end == QAudioFormat::BigEndian)
+        {
+            pValItem->setText(2, "BigEndian");
+        }
+        else
+        {
+            pValItem->setText(2, "LittleEndian");
+        }
+        pCapItem->addChild(pValItem);
     }
+    pDevice->addChild(pCapItem);
     
-    qDebug() << "channel counts:";
+    //qDebug() << "channel counts:";
+    pCapItem = new QTreeWidgetItem(pDevice);
+    pCapItem->setText(1, "Channel count");
+    
     foreach (int chcount, info.supportedChannelCounts())
     {
-        qDebug() << chcount;
+        //qDebug() << chcount;
+        QTreeWidgetItem *pValItem = new QTreeWidgetItem(pCapItem);
+        pValItem->setText(2, QString::number(chcount));
+        pCapItem->addChild(pValItem);
+        
     }
+    pDevice->addChild(pCapItem);
 
-    qDebug() << "frequencies:";
+    //qDebug() << "frequencies:";
+    pCapItem = new QTreeWidgetItem(pDevice);
+    pCapItem->setText(1, "Frequency");
+    
     foreach (int frequ, info.supportedFrequencies())
     {
-        qDebug() << frequ;
+        //qDebug() << frequ;
+        QTreeWidgetItem *pValItem = new QTreeWidgetItem(pCapItem);
+        pValItem->setText(2, QString::number(frequ));
+        pCapItem->addChild(pValItem);
     }
+    pDevice->addChild(pCapItem);
     
-    qDebug() << "sample rates:";
+    //qDebug() << "sample rates:";
+    pCapItem = new QTreeWidgetItem(pDevice);
+    pCapItem->setText(1, "Sample rate");
+    
     foreach (int samplerate, info.supportedSampleRates())
     {
-        qDebug() << samplerate;
+        //qDebug() << samplerate;
+        QTreeWidgetItem *pValItem = new QTreeWidgetItem(pCapItem);
+        pValItem->setText(2, QString::number(samplerate));
+        pCapItem->addChild(pValItem);
     }
+    pDevice->addChild(pCapItem);
     
-    qDebug() << "sample sizes:";
+    //qDebug() << "sample sizes:";
+    pCapItem = new QTreeWidgetItem(pDevice);
+    pCapItem->setText(1, "Sample size");
+    
     foreach (int samplesize, info.supportedSampleSizes())
     {
-        qDebug() << samplesize;
+        //qDebug() << samplesize;
+        QTreeWidgetItem *pValItem = new QTreeWidgetItem(pCapItem);
+        pValItem->setText(2, QString::number(samplesize));
+        pCapItem->addChild(pValItem);
     }
+    pDevice->addChild(pCapItem);
     
-    qDebug() << "sample types:";
-    foreach (int sampletype, info.supportedSampleTypes())
+    //qDebug() << "sample types:";
+    pCapItem = new QTreeWidgetItem(pDevice);
+    pCapItem->setText(1, "Sample type");
+    
+    foreach (QAudioFormat::SampleType sampletype, info.supportedSampleTypes())
     {
-        qDebug() << (int)sampletype;
+        //qDebug() << (int)sampletype;
+        QTreeWidgetItem *pValItem = new QTreeWidgetItem(pCapItem);
+        switch (sampletype)
+        {
+        case QAudioFormat::Unknown:
+            pValItem->setText(2, "Unknown");
+            break;
+        case QAudioFormat::SignedInt:
+            pValItem->setText(2, "SignedInt");
+            break;
+        case QAudioFormat::UnSignedInt:
+            pValItem->setText(2, "UnsignedInt");
+            break;
+        case QAudioFormat::Float:
+            pValItem->setText(2, "Float");
+            break;
+        }
+        pCapItem->addChild(pValItem);
     }
+    pDevice->addChild(pCapItem);
     
-    qDebug() << "codecs:";
+    //qDebug() << "codecs:";
+    pCapItem = new QTreeWidgetItem(pDevice);
+    pCapItem->setText(1, "Codec");
+    
     foreach (QString codect, info.supportedCodecs())
     {
-        qDebug() << codect;
+        //qDebug() << codect;
+        QTreeWidgetItem *pValItem = new QTreeWidgetItem(pCapItem);
+        pValItem->setText(2, codect);
+        pCapItem->addChild(pValItem);
     }
+    pDevice->addChild(pCapItem);
     
     
 }
