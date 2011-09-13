83e86a7fb9e2dbc393351c1ac4537c1e91988ed1
Ilkka Prusi <ilkka.prusi@gmail.com>
2011-09-13 04:26:33 +0300

    continued status and control handling

--- DecodeCtx.h
+++ DecodeCtx.h
@@ -30,46 +30,115 @@ protected:
     
     // TODO: check for necessary helpers during playback..
     
-    
     // current frame position in decode/playback
     uint64_t m_nCurrentFrame;
     
     // decode/playback frame size:
-    // channels*(samplesize/8) -> single frame in bytes
+    // channels * (samplesize/8) 
+    // -> single frame in bytes
     size_t m_nFrameSize;
     
     // count of frames:
     // filesize / framesize
     uint64_t m_nFrameCount;
     
+    // frames per second?
+    
     // bytes per second:
-    // filesize / (framesize*frequency)
+    // filesize / (framesize * sample rate)
     double m_dBytesPerSecond;
+
+    // channel count: amount of channels per frame
+    // sample size: sample width in bits    
+    void setFrameSize(const size_t nChannels, const size_t nSampleSize)
+    {
+        m_nFrameSize = nChannels * (nSampleSize/8);
+    }
+    // 
+    void setFrameCount(const uint64_t fileSize)
+    {
+        m_nFrameCount = (fileSize / m_nFrameSize);
+    }
+    // for simplistic buffer estimate:
+    // filesize in bytes
+    // sample rate in Hz
+    void setByteRate(const uint64_t fileSize, const double dSampleRate)
+    {
+        m_dBytesPerSecond = (fileSize / (m_nFrameSize * dSampleRate));
+    }
     
 public:
-    DecodeCtx() {}
+    DecodeCtx() 
+        : m_nCurrentFrame(0)
+        , m_nFrameSize(0)
+        , m_nFrameCount(0)
+        , m_dBytesPerSecond(0)
+    {}
     
+    void initialize(const uint64_t fileSize, const size_t nChannels, const size_t nSampleSize, const double dSampleRate)
+    {
+        setFrameSize(nChannels, nSampleSize);
+        setFrameCount(fileSize);
+        setByteRate(fileSize, dSampleRate);
+    }
 
     // set values to start (same as update(0))
-    void setBegin();
+    void setBegin()
+    {
+        m_nCurrentFrame = 0;
+    }
     
     // step forwards n frames
-    void stepFwd(int64_t i64Count = 0);
+    void stepFwd(int64_t i64Count = 0)
+    {
+        if ((m_nCurrentFrame + i64Count) < m_nFrameCount)
+        {
+            m_nCurrentFrame += i64Count;
+        }
+        else
+        {
+            // just set to end
+            m_nCurrentFrame = m_nFrameCount;
+        }
+    }
     
     // step backwards n frames
-    void stepBck(int64_t i64Count = 0);
+    void stepBck(int64_t i64Count = 0)
+    {
+        if (m_nCurrentFrame > i64Count)
+        {
+            m_nCurrentFrame -= i64Count;
+        }
+        else
+        {
+            // just set to start
+            m_nCurrentFrame = 0;
+        }
+    }
     
     // set values to start (same as update(length()))
-    void setEnd();
+    void setEnd()
+    {
+        m_nCurrentFrame = m_nFrameCount;
+    }
     
     // force absolute position
-    void updatePos(int64_t i64Pos);
+    void updatePos(const int64_t i64Pos)
+    {
+        m_nCurrentFrame = i64Pos;
+    }
     
-    // get current position
-    int64_t position();
+    // get current frame position
+    uint64_t position()
+    {
+        return m_nCurrentFrame;
+    }
     
-    // total length
-    int64_t length();
+    // total length in frames
+    uint64_t length()
+    {
+        return m_nFrameCount;
+    }
 };
 
 
